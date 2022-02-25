20dac5129a91a1f583da8ea3fc211ef54333105b
Amit <amitkr24031995@gmail.com>
2022-02-25 07:42:36 +0530

    Adding viewBag

---
+++ ViewBagDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagDemo.Models;
+
+namespace ViewBagDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from view bag !!";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+            string[] fruits = { "Apple", "Banana", "Grapes", "Orange" };
+            ViewBag.FruitsArray = fruits;
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket","Football","Hockey","Baseball"
+            };
+            Employee obj = new Employee();
+                obj.EmpId = 23;
+                obj.EmpName = "Krishna";
+                obj.EmpDesignation = "HouseWife";
+
+            ViewBag.Employee = obj;
+            ViewBag.CommonMessage = "This message is accessible by both viewData and ViewBag";
+            return View();
+        }
+    }
+}
